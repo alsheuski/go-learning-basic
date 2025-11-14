327ecc6f40b5ff94d28cb27d4bf9bb20d488670c
Alex Alsheuski <alex@alsheuski.com>
2025-11-14 20:28:13 +0300

    add getUserInput and convert function prototypes

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,7 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 	const usdToEur = 0.86
 	const usdToRub = 81
 	const eurToRub = 1 / usdToEur * usdToRub
+
+	userInput := getUserInput()
+}
+
+func getUserInput() string {
+	var userInput string
+
+	_, err := fmt.Scan(&userInput)
+	if err != nil {
+		fmt.Print("Ошибка ввода данных: ", err)
+		os.Exit(1)
+	}
+
+	return userInput
+}
+
+func convert(amount float64, from, to string) float64 {
+	return amount
 }
