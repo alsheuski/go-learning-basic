00b1eac2ea84bb5be7056ced9ab988d7efc45b89
Alex Alsheuski <alex@alsheuski.com>
2025-11-22 18:34:42 +0300

    add packages for project structure

--- 3-struct/main.go
+++ 3-struct/main.go
@@ -1,30 +1,4 @@
 package main
 
-import "time"
-
-type Bin struct {
-	ID        string
-	Private   bool
-	CreatedAt time.Time
-	Name      string
-}
-
-func NewBin(id string, private bool, createdAt time.Time, name string) *Bin {
-	return &Bin{
-		id,
-		private,
-		createdAt,
-		name,
-	}
-}
-
-type BinList struct {
-	List []Bin
-}
-
-func NewBinList(list []Bin) *BinList {
-	return &BinList{list}
-}
-
 func main() {
 }
